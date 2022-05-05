b8d2f886b6563a5e225e9b66d075bd1ba0702679
joonho lee <goodfluence@naver.com>
2022-05-05 20:13:17 +0900

    simple String test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -3,11 +3,35 @@ package study;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class StringTest {
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void split(){
+        String[] actual = "1,2".split(",");
+        assertThat(actual).containsExactly("1", "2");
+        assertThat(actual).contains("1", "2");
+    }
+
+    @Test
+    void substring(){
+        String actual = "(1,2)".substring(1, 4);
+        assertThat(actual).isEqualTo("1,2");
+    }
+
+    @Test
+    void charAt(){
+        char actual = "abc".charAt(1);
+        assertThat(actual).isEqualTo('b');
+        assertThatThrownBy(()->{
+
+        }).isInstanceOf(IndexOutOfBoundsException.class).hasMessageContaining("Index: \\d+, Size: \\d+");
+
     }
 }
